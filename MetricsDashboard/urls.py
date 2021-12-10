868e345c4d79b7ed141380f7ca54d4aaa2074b02
unknown <nghiale1004@gmail.com>
2021-12-10 17:55:01 +0700

    login

---
+++ MetricsDashboard/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', TemplateView.as_view(template_name='template/template.html')),
+    path('api/user/', include('users.urls', namespace='users')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
---
+++ react/blogapi/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import App from './App';
+import Header from './components/header';
+import Register from './components/register';
+import Login from './components/login';
+import Logout from './components/logout';
+
+const routing = (
+	<Router>
+		<React.StrictMode>
+			<Header />
+			<Routes>
+				<Route exact path="/welcome" element={<App/>} />
+				<Route path="/register" element={<Register/>} />
+				<Route path="/login" element={<Login/>} />
+				<Route path="/logout" element={<Logout/>} />
+			</Routes>
+		</React.StrictMode>
+	</Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
